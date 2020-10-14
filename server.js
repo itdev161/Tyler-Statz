4ba8eb7203e3d03e1e5ae7d967049edfa9a9de46
statzt <60333777+statzt@users.noreply.github.com>
2020-10-14 10:06:35 -0500

    Completed Activity 3

--- server.js
+++ server.js
@@ -1,57 +1,45 @@
 import express from 'express';
 import connectDatabase from './config/db';
 import { check, validationResult } from 'express-validator';
-import cors from 'cors';
 
-
+// Initialize express application
 const app = express();
 
-
+// Connect Database
 connectDatabase();
 
-
+// Configure Middleware
 app.use(express.json({ extended: false }));
-app.use(
-    cors({
-        origin: 'http://localhost:3000'
-    })
-);
 
-
-/** 
-*@route
-*@desc
-*/
-
-app.get('/', (req, res) =>
+// API end points
+/**
+ * @route GET/
+ * @desc Test endpoint
+ */
+app.get('/', (req,res) =>
     res.send('http get request sent to root api endpoint')
 );
 
 /**
-*@route
-*@desc
-*/
-app.post('/api/users', 
-[
-    check('name', 'Please enter your name')
-        .not()
-        .isEmpty(),
-    check('email', 'Please enter a valid email').isEmail(),
-    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
-],
-(req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(422).json({ errors: errors.array() });
-    } else {
-        return res.send(req.body);
+ * @route POST api/users
+ * @desc Register user
+ */
+app.post(
+    '/api/users',
+    [
+        check('name', 'Please enter your name').not().isEmpty(),
+        check('email', 'Please enter a valid email').isEmail(),
+        check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6})
+    ],
+    (req, res) =>{
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        } else {
+            return res.send(req.body);
+        }
     }
-}
-
 );
 
-const port = 5000;
-app.listen(port, () => console.log(`Express server running on port ${port}`));
-
-
-
+// Connection listener
+app.listen(3000, () => console.log(`Express server running on port 3000`));
