27e92a3abb9bfa1b9a826591ec008bfeea92d0e1
statzt <60333777+statzt@users.noreply.github.com>
2020-10-21 15:39:57 -0500

    Completed Activity 5

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
 import express from 'express';
 import connectDatabase from './config/db';
-import { check, validationResult } from 'express-validator';
+import {check, validationResult} from 'express-validator';
+import cors from 'cors';
+import bcrypt from 'bcryptjs';
+import User from './models/User';
+import jwt from 'jsonwebtoken';
+import config from 'config';
 
-// Initialize express application
+//start express app
 const app = express();
 
-// Connect Database
+//connect database
 connectDatabase();
 
-// Configure Middleware
-app.use(express.json({ extended: false }));
+//configure express
+app.use(express.json({extended: false}));
+app.use(
+    cors({
+        origin: 'http://localhost:3000'
+    })
+);
 
-// API end points
-/**
- * @route GET/
- * @desc Test endpoint
- */
-app.get('/', (req,res) =>
+app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
 );
 
@@ -29,17 +34,53 @@ app.post(
     [
         check('name', 'Please enter your name').not().isEmpty(),
         check('email', 'Please enter a valid email').isEmail(),
-        check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6})
+        check('password', 'Please enter a password with 6 or more characters').isLength({min: 6})
     ],
-    (req, res) =>{
+    async (req, res) => {
         const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(422).json({ errors: errors.array() });
-        } else {
-            return res.send(req.body);
-        }
-    }
-);
+        if(!errors.isEmpty()){
+            return res.status(422).json({errors: errors.array()});
+        }else{
+            const {name, email, password} = req.body;
+            try{
+                let user = await User.findOne({email: email});
+                if(user){
+                    return res
+                        .status(400)
+                        .json({errors: [{msg: 'User already exists'}]});
+                }
 
-// Connection listener
-app.listen(3000, () => console.log(`Express server running on port 3000`));
+                user = new User({
+                    name: name,
+                    email: email,
+                    password: password
+                });
+
+                const salt = await bcrypt.genSalt(10);
+                user.password = await bcrypt.hash(password, salt);
+
+                await user.save();
+                
+                const payload = {
+                    user: {
+                        id: user.id
+                    }
+                };
+
+                jwt.sign(
+                    payload,
+                    config.get('jwtSecret'),
+                    {expiresIn: '10hr'},
+                    (err, token) => {
+                        if(err) throw err;
+                        res.json({token: token});
+                    }
+                );
+            }catch(error){
+                res.status(500).send('Server error');
+            }
+        }
+})
+
+const port = 5000;
+app.listen(port, () => console.log(`Express server running on port ${port}`));
