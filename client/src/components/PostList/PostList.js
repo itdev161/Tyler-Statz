5537ab8bf25fc839d5fadd87d3b83fa0f6c7fc69
statzt <60333777+statzt@users.noreply.github.com>
2020-11-24 11:25:03 -0600

    Completed Activity 10

---
+++ client/src/components/PostList/PostList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PostListItem from './PostListItem';
+
+const PostList = props => {
+    const { posts, clickPost, deletePost, editPost } = props;
+    return posts.map(post => (
+        <PostListItem
+        key={post._id}
+        post={post}
+        clickPost={clickPost}
+        deletePost={deletePost}
+        />
+    ));
+};
+
+export default PostList;
---
+++ client/src/components/PostList/PostListItem.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import slugify from 'slugify';
+import './styles.css';
+
+const PostListItem = props => {
+    const { post, clickPost } = props;
+    const history = useHistory();
+
+    const handleClickPost = post => {
+        const slug = slugify(post.title, { lower: true });
+
+        clickPost(post);
+        history.push('/posts/${slug}');
+    };
+
+    const handleEditPost = post => {
+        EditPost(post);
+        history.push(`/edit-post/${post._is}`);
+    }
+
+    return (
+        <div>
+            <div className="postListItem" onClick={() => handleClickPost(post)}>
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+            </div>
+            <div className="postControls">
+                <button onClick={() => deletePost(post)}>Delete</button>
+                <button onClick={() => handleEditPost(post)}>Edit</button>
+            </div>
+        </div>
+    );
+};
+
+export default PostListItem;
